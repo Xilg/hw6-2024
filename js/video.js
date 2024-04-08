21325a67aaaf7fbb94da61411783e63fdc019cc4
CONNOR CHEN <connorc4796@gmail.com>
2024-04-07 22:30:07 -0400

    finished

--- js/video.js
+++ js/video.js
@@ -1,11 +1,66 @@
-var video;
+var video = document.querySelector("video"); 
+console.log("script initiated");
+function getVid(){
+	video = document.querySelector("video");
+	console.log("video found");
+	console.log(video)
+}
 
-window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+function playVid() { 
+	console.log("Play Video");
+	video.play();
+} 
 
-});
+function pauseVid() { 
+	console.log("Pause Video");
+	video.pause();
+} 
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+function decreaseSpeed() {
+	video.playbackRate = video.playbackRate * 0.8;
+	console.log("Speed is " + video.playbackRate);
+} 
 
+function increaseSpeed() {
+	video.playbackRate = video.playbackRate * 1.25;
+	console.log("Speed is " + video.playbackRate);
+} 
+
+function skipAhead() {
+	video.currentTime = video.currentTime + 60;
+	if (video.ended) {
+		video.load();
+		video.play();
+		playbackRate = 1;
+	}
+	console.log("Current location is " + video.currentTime);
+} 
+
+function mute() { 
+	if (video.muted) {
+		video.muted = false;
+		console.log("Unmuted");
+	}
+	else {
+		video.muted = true;
+		console.log("Muted");
+	}
+}
+
+function changeVolume() {
+	console.log(volumeSlider.value);
+	var slider_val = document.querySelector('#volume').innerHTML; 
+	slider_val = volumeSlider.value;
+	video.volume = slider_val / 100;
+	document.querySelector('#volume').innerHTML = slider_val + '%';
+}
+	
+function gray() { 
+	video.classList.add("oldSchool");
+	console.log("In grayscale");
+}
+
+function color() {
+	video.classList.remove("oldSchool");
+	console.log("In color");
+}
